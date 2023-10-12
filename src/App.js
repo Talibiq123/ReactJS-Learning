eddc4eef37db876e5c239f13cd6625c0d7a356b4
Talibiq123 <talibsaeed90@gmail.com>
2023-10-12 19:41:49 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-import Video from './component/Video';
+import "./App.css";
+import Video from "./component/Video";
 
 function App() {
+  let obj = {
+    title: "React JS tutorial",
+    views: "999K",
+    time: "1 year ago",
+    channel: "Coder Dost",
+  };
   return (
-    <div>
-    Hello
-    <Video></Video>
+    <div className="App">
+      <div>Videos</div>
+      <Video {...obj}></Video>
+      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video
+        title="Mongo DB tutorial"
+        views="1M"
+        time="1 month ago"
+        channel="Coder Dost"
+      ></Video>
     </div>
   );
 }
