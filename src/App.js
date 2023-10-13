b702f99db086312d622f0f702bb37e01b95363f2
Talibiq123 <talibsaeed90@gmail.com>
2023-10-13 13:44:37 +0530

    condtional rendering

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import Video from "./component/Video";
 
 function App() {
   let obj = {
+    id: 1,
     title: "React JS tutorial",
     views: "999K",
     time: "1 year ago",
     channel: "Coder Dost",
+    verified: false,
   };
   return (
     <div className="App">
       <div>Videos</div>
       <Video {...obj}></Video>
-      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video id="3" title="Node JS tutorial" views="100K" time="1 month ago" verified={true}></Video>
       <Video
+        id = "2"
         title="Mongo DB tutorial"
         views="1M"
         time="1 month ago"
         channel="Coder Dost"
+        verified={false}
       ></Video>
     </div>
   );
--- src/component/Video.js
+++ src/component/Video.js
@@ -1,14 +1,21 @@
 import "./Video.css";
 
-function Video({ title, channel = "Coder Dost", views, time }) {
+function Video({id, title, channel = "Coder Dost", views, time, verified}) {
+  let channelJSX;
+  if(verified) {
+    channelJSX = <div className="channel">{channel} ✅</div>;
+  } else {
+    channelJSX = <div className="channel">{channel} </div>;
+  }
+
   return (
     <>
       <div className="container">
         <div className="pic">
-          <img src="https://picsum.photos/640/360" alt="Katherine Johnson" />
+          <img src={`https://picsum.photos/id/${id}/160/90`} alt="Katherine Johnson" />
         </div>
         <div className="title">{title}</div>
-        <div className="channel">{channel}</div>
+        {channelJSX}
         <div className="views">
           {views} views <span>.</span> {time}
         </div>
