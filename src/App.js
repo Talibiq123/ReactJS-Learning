4044911aba5d3a4db50aff34fe801f1b1ad8ef50
Talibiq123 <talibsaeed90@gmail.com>
2023-10-16 19:17:57 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Video from "./component/Video";
 
 function App() {
   let obj = {
+    verified: true,
     title: "React JS tutorial",
     views: "999K",
     time: "1 year ago",
@@ -12,8 +13,9 @@ function App() {
     <div className="App">
       <div>Videos</div>
       <Video {...obj}></Video>
-      <Video title="Node JS tutorial" views="100K" time="1 month ago"></Video>
+      <Video verified={false} title="Node JS tutorial" views="100K" time="1 month ago"></Video>
       <Video
+        verified={true}
         title="Mongo DB tutorial"
         views="1M"
         time="1 month ago"
