4b6838df30e356a6f2d2108a0545be6d5d9271b4
Talibiq123 <talibsaeed90@gmail.com>
2023-10-22 10:57:53 +0530

    Lifting Up State

--- src/App.js
+++ src/App.js
@@ -1,43 +1,28 @@
-import "./App.css";
-import PlayButton from "./component/PlayButton";
-import Video from "./component/Video";
-import { useState } from "react";
-import videoDB from "./data/data";
-import AddVideo from "./component/AddVideo";
-/*import Counter from "./component/Counter";*/
+import { useState } from 'react';
+import './App.css';
+import AddVideo from './component/AddVideo';
+import videoDB from './data/data';
+import VideoList from './component/VideoList';
 
 
 function App() {
+  console.log('render App')
 
-  const [videos, setVideos] = useState(videoDB);
+  const [videos,setVideos] = useState(videoDB);
+
+  function addVideos(video){
+      setVideos([
+            ...videos,
+            {...video, id: videos.length+1}
+          ]);
+  }
 
   return (
-    <div className="App">
+    <div className="App" onClick={()=>console.log('App')}>
+       <AddVideo addVideos={addVideos}></AddVideo>
+       <VideoList videos={videos}></VideoList>
 
-      <AddVideo addVideos={setVideos}></AddVideo>
 
-      {videos.map((video) => (
-        <Video
-          key={video.id}
-          verified={video.verified}
-          title={video.title}
-          views={video.views}
-          time={video.time}
-          channel={video.channel}
-          id={video.id}
-        >
-          <PlayButton
-            onPlay={() => console.log("play", video.title)}
-            onPause={() => console.log("pause", video.title)}
-          >
-            {video.title}
-          </PlayButton>
-        </Video>
-      ))}
-      <div style={{ clear: "both" }}>
-        {/*<PlayButton message="pause-msg" onClick={() => console.log('pause')}>Pause</PlayButton>*/}
-      </div>
-      {/*<Counter></Counter>*/}
     </div>
   );
 }
--- src/component/AddVideo.js
+++ src/component/AddVideo.js
@@ -1,25 +1,27 @@
-import "./AddVideo.css";
-import { useState } from "react";
+import './AddVideo.css';
+import {useState} from 'react';
+
+const initialState = {
+    time: '1 month ago',
+    channel: 'Coder Dost',
+    verified: true,
+    title:'',
+    views:''
+  }
 
 function AddVideo({addVideos}) {
-  const [video, setVideo] = useState({
-    verified: true,
-    time: "7 months ago",
-    channel: "Coder Dost",
-  });
+  const [video, setVideo] = useState(initialState);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(video);
-    addVideos()
-  }
+    addVideos(video)
+    setVideo(initialState)
 
+  }
   function handleChange(e) {
-    console.log(e.target.name, e.target.value);
     setVideo({...video,
-      [e.target.name]: e.target.value
+        [e.target.name] : e.target.value
     })
-    
   }
 
   return (
@@ -29,28 +31,17 @@ function AddVideo({addVideos}) {
         name="title"
         onChange={handleChange}
         placeholder="title"
+        value={video.title}
       />
       <input
         type="text"
         name="views"
         onChange={handleChange}
         placeholder="views"
+        value={video.views}
       />
       <button
         onClick={handleSubmit}
-        // onClick={() => {
-        //   setVideos([
-        //     ...videos,
-        //     {
-        //       id: videos.length + 1,
-        //       verified: true,
-        //       title: "ExpressJS tutorial",
-        //       views: "830K",
-        //       time: "7 months ago",
-        //       channel: "Coder Dost",
-        //     },
-        //   ]);
-        // }}
       >
         Add Video
       </button>
