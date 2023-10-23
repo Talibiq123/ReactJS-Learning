da538803120add23ea16b1d4fcab080efd3dc1b3
Talibiq123 <talibsaeed90@gmail.com>
2023-10-23 13:20:39 +0530

    CRUD Operations

--- src/component/VideoList.js
+++ src/component/VideoList.js
@@ -1,7 +1,7 @@
 import Video from "./Video";
 import PlayButton from "./PlayButton";
 
-function VideoList({videos}){
+function VideoList({videos,deleteVideo,editVideo}){
 
     return(
         <>
@@ -14,6 +14,8 @@ function VideoList({videos}){
               channel={video.channel}
               verified={video.verified}
               id={video.id}
+              deleteVideo={deleteVideo}
+              editVideo={editVideo}
             >
               <PlayButton
                 onPlay={() => console.log('Playing..',video.title)}
@@ -27,4 +29,4 @@ function VideoList({videos}){
     )
 }
 
-export default VideoList;
+export default VideoList
