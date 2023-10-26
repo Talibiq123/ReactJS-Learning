b5d3e9dca580f8d2631de47ff586bf5f9416f9a5
Talibiq123 <talibsaeed90@gmail.com>
2023-10-26 14:03:31 +0530

    Context API

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
-import { useReducer, useState } from 'react';
+import {useReducer, useState} from 'react';
 import './App.css';
 import AddVideo from './component/AddVideo';
 import videoDB from './data/data';
 import VideoList from './component/VideoList';
+import ThemeContext from './context/ThemeContext';
+
+
 function App() {
-  console.log('render App');
-
+  console.log('render App')
   const [editableVideo,setEditableVideo] = useState(null);
-
-  function videoReducer(videos, action) { 
-    switch(action.type) {
-      case 'ADD' :
+  const [mode,setMode] = useState('darkMode')
+  function videoReducer(videos,action){
+    switch(action.type){
+      case 'ADD':
         return [
-            ...videos,
-            {...action.payload, id: videos.length+1}
-          ]
-      case 'DELETE' : 
-        return videos.filter(video=>video.id!==action.payload);
-      case 'UPDATE' :
-        const index = videos.findIndex(v => v.id === action.payload.id);
+          ...videos,
+          {...action.payload, id: videos.length+1}
+        ]
+      case 'DELETE':
+        return videos.filter(video=>video.id!==action.payload)  
+      case 'UPDATE':
+        const index = videos.findIndex(v=>v.id===action.payload.id)
         const newVideos = [...videos]
-        newVideos.splice(index, 1, action.payload);
+        newVideos.splice(index,1,action.payload)
         setEditableVideo(null);
         return newVideos;
-      default :
-      return videos; 
+      default:
+        return videos  
     }
-  }
 
-  const [videos, dispatch] = useReducer(videoReducer, videoDB);
+  }
+  const [videos,dispatch] = useReducer(videoReducer,videoDB)
+
+
+
 
   function editVideo(id){
     setEditableVideo(videos.find(video=>video.id===id))
   }
 
-
-
   return (
-    <div className="App" onClick={()=>console.log('App')}>
+    <ThemeContext.Provider value={mode}>
+    <div className={`App ${mode}`} onClick={()=>console.log('App')}>
+       <button onClick={()=>setMode(mode==='darkMode'? 'lightMode':'darkMode')}>Mode</button>
        <AddVideo dispatch={dispatch} editableVideo={editableVideo}></AddVideo>
        <VideoList dispatch={dispatch} editVideo={editVideo}  videos={videos}></VideoList>
 
-
     </div>
+    </ThemeContext.Provider>
+
   );
 }
 
